eb44ca605bc442b0392505af50aad4ff44b0f749
Ethan Fischer <ethan.f.fischer@gmail.com>
2015-06-17 13:24:35 -0500

    Completed the Assignment
    Yes!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL                                 # inv is the variable that will hold the inverse
+    
+    set <- function(y)                          # creates the matrix
+    {
+          x <<- y                             
+          inv <<- NULL
+    }
+    get <- function() x                         # returns the matrix
+    setinv <- function(nvrs) inv <<- nvrs       # sets x's mean
+    getinv <- function() inv                    # returns x's mean
+    
+    list(set = set, get = get,                  # list of each function
+    setinv = setinv,
+    getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix"
+# returned by makeCacheMatrix above. If the inverse has already been
+# calculated (and the matrix has not changed), then cacheSolve should
+# retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()                       # store x's inv in 'm'
+    if(!is.null(inv)) {                     # if x$getinv returned a value, return it
+        message("getting cached data")
+        return(inv)                         # return the cached mean and exit cachemean()
+    }
+    data <- x$get()                         # otherwise, store the matrix in data
+    inv <- solve(data, ...)                 # calculate inverse of x and store it in 'm'
+    x$setinv(inv)                           # set the inverse of x for future use
+    inv                                     # return the inverse
 }
